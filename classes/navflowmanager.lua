eeadf2766587764b1478b20cdc0945b50ff338aa
88fa76d8d91b3d638130aa60431242acb46f92d5 <1032176412@pfur.ru>
2019-11-12 12:43:11 +0300

    gitignore upd; tweak flowmanager

--- classes/navflowmanager.lua
+++ classes/navflowmanager.lua
@@ -1,7 +1,19 @@
-log("this class should be loaded")
 ---@class NavFlowManager
 NavFlowManager = NavFlowManager or class()
 
 function NavFlowManager:init()
+    ---@alias flow_index number
+    ---@type table<number, flow_index> @segment to navflow map
+    self._nav_flow = {}
+    ---@alias nav_flow table
+    ---@type nav_flow[] @positional index of flow_data along the flow
+    self._flow_distance = {}
+end
 
+function NavFlowManager:set_flow_distance(flow_dist)
+    self._flow_distance = flow_dist
+end
+
+function NavFlowManager:flow_distance()
+    return self._flow_distance
 end
